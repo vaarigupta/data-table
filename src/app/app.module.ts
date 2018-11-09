d2681a0351373de3558ba8ac931248b6fe105792
vaarigupta <vaarigupta24@gmail.com>
2018-11-09 22:59:10 +0530

    data table added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {MatButtonModule} from '@angular/material/button';
+import { DataTableComponent } from './data-table/data-table.component';
+import { MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataTableComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
   BrowserAnimationsModule,
-  MatButtonModule
+  MatButtonModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
